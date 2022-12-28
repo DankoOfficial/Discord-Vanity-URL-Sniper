0d700132f1929b777a1210653f3108fb27334a34
DankoOfficial <99405955+DankoOfficial@users.noreply.github.com>
2022-12-28 14:18:30 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from requests import patch, get
-import os
+import os, time
 
 class Stats:
     faliures = 0
@@ -15,10 +15,16 @@ def checkInvite(invite):
         return "Error"
 
 def snipe(server,token,vanity):
-    patch(f'https://discord.com/api/v9/guilds/{server}/vanity-url',json={"code":vanity},headers={'authorization':token})
-    print("[+] Vanity Sniped")
-    Stats.sniped = True
-    return True
+    x = patch(f'https://discord.com/api/v9/guilds/{server}/vanity-url',json={"code":vanity},headers={'authorization':token})
+    if 'Invite code is either invalid or taken.' in x.text:
+        Stats.faliures+=1
+        print("Sleeping for 15 seconds...")
+        time.sleep(15)
+        prg()
+    else:
+        print("[+] Vanity Sniped")
+        Stats.sniped = True
+        return True
 
 def tokenCheck(token):
     r = get('https://discord.com/api/v9/users/@me',headers={'authorization':token}).text
@@ -37,12 +43,13 @@ while True:
         break
     else:
         print("Invalid token..")
-
-while True:
-    if not checkInvite(vanity):
-        Stats.faliures+=1
-        os.system(f'title Failed to snipe - {Stats.faliures} / Vanity Sniped - {Stats.sniped}')
-    elif checkInvite(vanity):
-        if snipe(serverid,token,vanity):
-            break
-input()
+def prg():
+    while True:
+        if not checkInvite(vanity):
+            Stats.faliures+=1
+            os.system(f'title Failed to snipe - {Stats.faliures} / Vanity Sniped - {Stats.sniped}')
+        elif checkInvite(vanity):
+            if snipe(serverid,token,vanity):
+                break
+    input()
+prg()
